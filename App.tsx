7ed59e7907e3dba156ede89986041b7cb5901275
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2023-10-10 19:18:10 +0300

    Path aliases

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import Button from "@/components/button";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app !</Text>
+      <Button />
       <StatusBar style="auto" />
     </View>
   );
