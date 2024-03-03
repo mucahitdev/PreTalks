02fa7aacbb3960f648c199708128943808d4bd15
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2024-03-04 02:44:42 +0300

    core:submit-workflows

---
+++ scripts/useBuildNumberEnv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# outputIos=$(eas build:version:get -p ios)
+# outputAndroid=$(eas build:version:get -p android)
+currentIosVersion=${outputIos#*buildNumber - }
+currentAndroidVersion=${outputAndroid#*versionCode - }
+
+# PRETALKS_IOS_BUILD_NUMBER=$((currentIosVersion+1))
+PRETALKS_ANDROID_VERSION_CODE=$((currentAndroidVersion+1))
+
+# bash -c "PRETALKS_IOS_BUILD_NUMBER=$PRETALKS_IOS_BUILD_NUMBER PRETALKS_ANDROID_VERSION_CODE=$PRETALKS_ANDROID_VERSION_CODE $*"
+bash -c "PRETALKS_ANDROID_VERSION_CODE=$PRETALKS_ANDROID_VERSION_CODE $*"
