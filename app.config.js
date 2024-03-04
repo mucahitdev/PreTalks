4069aa25cae5fdd27e39b7c4daa0194e15a6b5b0
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2024-03-04 03:44:13 +0300

    set app config

--- app.config.js
+++ app.config.js
@@ -33,8 +33,40 @@ module.exports = function (config) {
     expo: {
       version: VERSION,
       //   splash: SPLASH_CONFIG,
+      owner: "pixel-team",
       runtimeVersion: {
         policy: "appVersion",
+      },
+      name: "PreTalks",
+      slug: "pretalks",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      userInterfaceStyle: "automatic",
+      splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+
+      assetBundlePatterns: ["**/*"],
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: "com.koksoft.pretalks",
+      },
+      android: {
+        package: "com.koksoft.pretalks",
+        adaptiveIcon: {
+          foregroundImage: "./assets/icon.png",
+          backgroundColor: "#ffffff",
+        },
+      },
+      web: {
+        favicon: "./assets/favicon.png",
+      },
+      extra: {
+        eas: {
+          projectId: "1ee2d6b3-6c96-405b-9761-b403914e663a",
+        },
       },
       //   ios: {
       // splash: {
@@ -55,6 +87,27 @@ module.exports = function (config) {
       //   dark: DARK_SPLASH_CONFIG,
       // },
       //   },
+      updates: {
+        enabled: true,
+        fallbackToCacheTimeout: 1000,
+        url: "https://u.expo.dev/1ee2d6b3-6c96-405b-9761-b403914e663a",
+      },
+      experiments: {
+        tsconfigPaths: true,
+      },
+      plugins: [
+        "@react-native-firebase/app",
+        "@react-native-firebase/crashlytics",
+        [
+          "expo-build-properties",
+          {
+            ios: {
+              useFrameworks: "static",
+            },
+          },
+        ],
+        "expo-font",
+      ],
     },
   };
 };
